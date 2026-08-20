181172d70875fe52ef804ad01b3b249b5c5dc9be
César <CsarGiao@>
2026-08-19 22:10:29 -0300

    aula 19 08

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full' //manda direto para o login
+  },
+  {
+  path: 'login',
+  loadComponent: () => import('./page/login/login.page').then(m => m.LoginPage)
   }
 ];
 @NgModule({
--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonList,
+  IonBadge, 
+  IonLabel
+} from '@ionic/angular/standalone';
+import { IonIcon } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonItem,
+    IonInput,
+    IonButton,
+    IonList,
+    IonBadge,
+    IonLabel
+  ]
 })
-export class LoginPage  implements OnInit {
-
+export class LoginPage implements OnInit {
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() { }
 }
